cc6f0ed11b20f7c13b257a610ef848695abed7f6
mosaladtaooo <zhantaolau54@gmail.com>
2024-11-15 14:43:30 +0800

    update

---
+++ Assets/Script/InventorySystem.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+
+    public static InventorySystem Instance { get; set; }
+
+    public GameObject inventoryScreenUI;
+    public bool isOpen;
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+
+    void Start()
+    {
+        isOpen = false;
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.I) && !isOpen)
+        {
+
+            Debug.Log("i is pressed");
+            inventoryScreenUI.SetActive(true);
+            isOpen = true;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.I) && isOpen)
+        {
+            inventoryScreenUI.SetActive(false);
+            isOpen = false;
+        }
+    }
+
+}
--- Assets/Script/SelectionManager.cs
+++ Assets/Script/SelectionManager.cs
@@ -1,73 +1,61 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SelectionManager : MonoBehaviour
 {
-    public static SelectionManager Instance {  get; set; }
+    public static SelectionManager Instance { get;set;}
     public bool onTarget;
-    public bool rayhit;
-    public GameObject interaction_info_UI;
+    public GameObject interaction_Info_UI;
     Text interaction_text;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        rayhit = false;
         onTarget = false;
-        interaction_text = interaction_info_UI.GetComponent<Text>();
-        //把interaction text定義爲GameObject interaction_info_UI裏面的text
+        interaction_text = interaction_Info_UI.GetComponent<Text>();
     }
 
     private void Awake()
     {
-        if (Instance != null && Instance != this)
+        if (Instance != null&&Instance != this) 
         {
-
             Destroy(gameObject);
         }
         else
         {
             Instance = this;
+
+
+
         }
 
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);  
-        //從中間蛇一個綫
-        RaycastHit hit;  //射綫hit到東西的狀態
-        if (Physics.Raycast(ray, out hit)) 
-            //這是一個bool,檢測射綫有沒有射到東西,這裏是IF有hit到東西
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
         {
-            rayhit=true;
-            var selectionTransform = hit.transform;  
-            //把hit的transform(位置)名爲selectiontransform,就是我們選擇的物件位置
+            var selectionTransform = hit.transform;
 
-            if (selectionTransform.GetComponent<InteractableObject>()&&selectionTransform.GetComponent<InteractableObject>().playerinRange)
+            if (hit.transform.GetComponent<InteractableObject>()&& hit.transform.GetComponent<InteractableObject>().playerinRange)
             {
-                onTarget= true;
-                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
-                interaction_info_UI.SetActive(true);
+                onTarget=true;
+                interaction_text.text = hit.transform.GetComponent<InteractableObject>().GetItemName();
+                //interaction_Info_UI.SetActive(true);
             }
             else
             {
-                onTarget= false;
-                interaction_info_UI.SetActive(false);
+                //interaction_Info_UI.SetActive(false);
             }
 
         }
         else
         {
-            rayhit = false;
-            onTarget = false;
-            interaction_info_UI.SetActive(false);
+            //interaction_Info_UI.SetActive(false);
         }
-
     }
 }
